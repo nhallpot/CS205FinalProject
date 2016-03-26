d692169813f840a1c40aaaaebeb66450ba98ff97
nhallpot <nhallpot@uvm.edu>
2016-03-26 19:58:23 -0400

    top.php is now "connecting to database"... need to run queries

--- top.php
+++ top.php
@@ -1,27 +1,52 @@
+<?php
+include "constants.php";
+require_once('custom-functions.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Sorry!</title>
         <meta charset="utf-8">
-        <meta name="description" content="A php app that tracks inventory">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="custom.css" type="text/css" media="screen">    
-        <link rel="stylesheet" href="bootstrap.css" type="text/css" media="screen">
-        <link rel="stylesheet" href="bootstrap.min.css" type="text/css" media="screen">
-       
-        <?php
-        $debug = false;
+        <meta name="author" content="Mark me wrong">
+        <meta name="description" content="Sorry! game implementation">
 
-// %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
-//
-// PATH SETUP
-//
-        $domain = "http://";
-        if (isset($_SERVER['HTTPS'])) {
-            if ($_SERVER['HTTPS']) {
-                $domain = "https://";
-            }
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+        <![endif]-->
+
+
+        <?php
+        $debug = true;
+
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        //
+        // inlcude all libraries. Note some are in lib and some are in bin
+        // bin should be located at the same level as www-root (it is not in 
+        // github)
+        // 
+        // yourusername
+        //     bin
+        //     www-logs
+        //     www-root
+        
+        require_once('security.php');
+        
+        require_once('Database.php');
+        
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        //
+        // PATH SETUP
+        //  
+  
+        // sanitize the server global variable
+        $_SERVER = filter_input_array(INPUT_SERVER, FILTER_SANITIZE_STRING);
+        foreach ($_SERVER as $key => $value) {
+            $_SERVER[$key] = sanitize($value, false);
         }
+        
+        $domain = "//"; // let the server set http or https as needed
 
         $server = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, "UTF-8");
 
@@ -38,11 +63,70 @@
             print_r($path_parts);
             print "</pre>";
         }
+        
+        $yourURL = $domain . $phpSelf;
+
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        // sanatize global variables 
+        // function sanitize($string, $spacesAllowed)
+        // no spaces are allowed on most pages but your form will most likley
+        // need to accept spaces. Notice my use of an array to specfiy whcih 
+        // pages are allowed.
+        // generally our forms dont contain an array of elements. Sometimes
+        // I have an array of check boxes so i would have to sanatize that, here
+        // i skip it.
+
+        $spaceAllowedPages = array("home.php");
+
+        if (!empty($_GET)) {
+            $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
+            foreach ($_GET as $key => $value) {
+                $_GET[$key] = sanitize($value, false);
+            }
+        }
+        
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        //
+        // Process security check.
+        //
+        
+        // if (!securityCheck($path_parts, $yourURL)) {
+        //     print "<p>Login failed: " . date("F j, Y") . " at " . date("h:i:s") . "</p>\n";
+        //     die("<p>Sorry you cannot access this page. Security breach detected and reported</p>");
+        // }
+
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        //
+        // Set up database connection
+        //
+        print(get_current_user());
+        
+        $dbUserName = get_current_user() . '_reader';
+        $whichPass = "r"; //flag for which one to use.
+        $dbName = DATABASE_NAME;
+        if($debug)
+        {
+            print('<p>testing</p>');
+            print($dbUserName);
+            print($whichPass);
+            print($dbName);
+        }
+
+        $thisDatabaseReader = new Database($dbUserName, $whichPass, $dbName);
+        
+        $dbUserName = get_current_user() . '_writer';
+        $whichPass = "w";
+        $thisDatabaseWriter = new Database($dbUserName, $whichPass, $dbName);
+        
+
+
+        ?>  
+
+
 
     </head>
-    <!-- ################ body section ######################### -->
 
+    <!-- **********************     Body section      ********************** -->
     <?php
-    print '<body>';
-    //id="' . $path_parts['filename'] . '"
+    print '<body>'
     ?>
