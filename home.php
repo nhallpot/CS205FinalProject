80335006835498bc10b13b4752edff5920513694
nhallpot <nhallpot@uvm.edu>
2016-03-30 12:33:04 -0400

    attempting to run a query

--- home.php
+++ home.php
@@ -1,9 +1,13 @@
 	<?php
-	/*
+	
 	include 'top.php';
-	*/
+	
 	?>
-	<p> testing </p>
-
+	<?php
+		print("<h1> start of attempt to query </h1>");
+		$query = "SELECT * FROM PIECE;";
+	    $numRows = $thisDatabaseReader->select($query,null);
+		print("<p>".$numRows[0]["isSafety"]."</p>");
+	?>
     </body>
 </html>
