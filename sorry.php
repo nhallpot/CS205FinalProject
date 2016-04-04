4e4050860bd2084305b7c188adb26cfd59d42b11
nhallpot <nhallpot@uvm.edu>
2016-04-04 11:37:45 -0400

    added some base files, logo for header, and nav links

---
+++ sorry.php
@@ -0,0 +1,16 @@
+<?php
+
+//##############################################################################
+//
+// This is where all of the game logic will go. Separate files for computer easy and computer hard. 
+// All of the database calls from post requests will be here
+// 
+//##############################################################################
+include "top.php";
+
+// Begin output
+print '<article>';
+print '<h2>Sample Page</h2>';
+print '</article>';
+include "footer.php";
+?>
