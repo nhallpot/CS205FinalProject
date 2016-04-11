bbb613f7a893adae186e3fc6614b70947cf43e8c
nhallpot <nhallpot@uvm.edu>
2016-04-11 09:15:29 -0400

    Fixed some bugs

--- header.php
+++ header.php
@@ -1,12 +1,9 @@
 <!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
 <header>
-<<<<<<< HEAD
 	<h1> Info </h1>
-	<?php
+<!-- 	<?php
 		include "info.php";
-	?>
-=======
+	?> -->
     <a href="index.php"><img alt="Sorry! logo" src="images/sorry.jpg"></a>
->>>>>>> refs/remotes/origin/master
 </header>
 <!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
--- nav.php
+++ nav.php
@@ -4,37 +4,27 @@
         <?php
         // This sets the current page to not be a link. Repeat this if block for
         //  each menu item 
-        
-        
         if ($path_parts['filename'] == "index") {
-            print '<li><a class="activePage" href="index.php">Home</a></li>';
+            print('<li><a class="activePage" href="index.php">Home</a></li>');
         } else {
             print '<li><a href="index.php">Home</a></li>';
         }
         
-<<<<<<< HEAD
         if ($path_parts['filename'] == "stats") {
-            print '<li><a class="activePage" href="stats.php">Stats</a></li>';
+            print('<li><a class="activePage" href="stats.php">Stats</a></li>');
         } else {
-            print '<li><a href="stats.php">Stats</a></li>';
+            print ('<li><a href="stats.php">Stats</a></li>');
         }
         
         if ($path_parts['filename'] == "help") {
-            print '<li><a class="activePage" href="help.php">Help</a></li>';
+            print ('<li><a class="activePage" href="help.php">Help</a></li>');
         } else {
-            print '<li><a href="help.php">Help</a></li>';
-=======
-        if ($path_parts['filename'] == "help") {
-            print '<li class="activePage"><a href="help.php">Help</a></li>';
-        } else {
-            print '<li><a href="help.php">Help</a></li>';
+            print ('<li><a href="help.php">Help</a></li>');
         }
-        
         if ($path_parts['filename'] == "sorry") {
-            print '<li class="activePage"><a href="sorry.php">Play Sorry!</a></li>';
+            print ('<li class="activePage"><a href="sorry.php">Play Sorry!</a></li>');
         } else {
-            print '<li><a href="sorry.php">Play Sorry!</a></li>';
->>>>>>> refs/remotes/origin/master
+            print('<li><a href="sorry.php">Play Sorry!</a></li>');
         }
         ?>
     </ol>
