37680c26d796e44b5ea231573b41991ea75e5f4b
Noah Hall-Potvin <nhallpot@uvm.edu>
2016-04-12 12:18:07 -0400

    added wiki link for how to play the game. should be sufficient

--- help.php
+++ help.php
@@ -9,7 +9,8 @@ include "top.php";
 
 // Begin output
 print '<article>';
-print '<h2>Sample Page</h2>';
+print('<h4> Welcome to the help page! Here you can find a very helpful link that explains how to play Sorry!');
+print("<h6><a target='_blank' href='https://en.wikipedia.org/wiki/Sorry!_(game)'> Click this link to see the Wikipedia instructions for playing Sorry!</a></h6>");
 print '</article>';
 include "footer.php";
 ?>
