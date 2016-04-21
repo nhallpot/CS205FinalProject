c203b449f316c07ed1decfb458d6f7580a562cfc
lukelakea <lukelakea@gmail.com>
2016-04-21 15:24:41 -0400

    trying to merge, help

--- index.php
+++ index.php
@@ -7,8 +7,33 @@
 //##############################################################################
 include "top.php";
 ?>
+	<script type="text/javascript">
+         <!--
+            var imgObj = null;
+            
+            function init(){
+               imgObj = document.getElementById('myImage');
+               imgObj.style.position= 'relative'; 
+               imgObj.style.left = '0px'; 
+            }
+            
+            function moveRight(imgName){
+               imgObj=document.getElementByClass(imgName);
+               imgObj.style.position='relative';
+               imgObj.style.left = parseInt(imgObj.style.left) + 200 + 'px';
+            }
+            
+            window.onload =init;
+         //-->
+    </script>
+    <form id="updateButton">
+         <input type="button" value="Click Me" onclick="moveRight();" />
+      </form>
 
 	<article>
+
+	
+
 		<!-- Pawn test <figure class = "greenHome">
 			<img src="images/redPawn.png">
 		</figure> -->
