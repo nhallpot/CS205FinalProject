3db5b01b44d0376215ce9f806fe72663c03140e4
Noah Hall-Potvin <nhallpot@uvm.edu>
2016-04-26 11:41:32 -0400

    added helpful links in index, more info on how to play our implementation in help

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 //##############################################################################
 include "top.php";
 ?>
-
+<article>
+	<h1> Welcome to our web-based implementation of the game Sorry! </h1>
+	<h3> We completed this project for our CS 205 Final Project, and we hope you have fun playing the game Sorry on the INTERNET!!!! </h3>
+</article>
 <?php
 	include "footer.php";
 ?>
