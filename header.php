774d9963b0945e364aa1392a0be187378d1c6266
Noah Hall-Potvin <nhallpot@uvm.edu>
2016-04-27 09:01:59 -0400

    Revert "Merge pull request #72 from nhallpot/pr/70"
    This reverts commit 322d70f49e5558d77fca85d2c39c96ef778207a1, reversing
    changes made to 5c88633308f36a90aff8cff0aa69062f12771066.

--- header.php
+++ header.php
@@ -1,26 +1,9 @@
 <!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
 <header>
-<<<<<<< HEAD
-<<<<<<< HEAD
-	<h1 id ="info"> Info </h1>
-=======
-<<<<<<< HEAD
-	<h1 id ="info"> Welcome to Sorry! </h1>
-=======
-<<<<<<< HEAD
-	<h1> Sorry! </h1>
-=======
-	<h1 id ="info"> Info </h1>
->>>>>>> nhallpot/master
->>>>>>> refs/remotes/nhallpot/master
->>>>>>> refs/remotes/nhallpot/master
-<!-- 	<?php
-=======
 
 	<h1 id ="info"> Welcome to Sorry! </h1>
 	
 <?php
->>>>>>> refs/remotes/origin/master
 		include "info.php";
 ?> 
     
